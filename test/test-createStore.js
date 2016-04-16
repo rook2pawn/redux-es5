aa1f66196816149e2662bda7ebc5805f4bd6a5bd
David Wee <rook2pawn@gmail.com>
2016-04-16 13:16:19 -0700

    [test] more createStore test coverage

--- test/test-createStore.js
+++ test/test-createStore.js
@@ -1,5 +1,6 @@
-var store = require('../createStore')
-var test = require('tape')
+var createStore = require('../createStore');
+var combineReducers = require('../combineReducers');
+var test = require('tape');
 
 
 test('check that store defers default state to the reducer', function(t) {
@@ -14,5 +15,73 @@ test('check that store defers default state to the reducer', function(t) {
       break;
     }
   }
-  t.deepEquals(store(reducer).getState(),[])
+  t.deepEquals(createStore(reducer).getState(),[])
+})
+
+
+test('getState - dispatch test', function(t) {
+  t.plan(3)
+  var reducer = function(state,action) {
+    if (state === undefined) {
+      state = []
+    }
+    switch (action.type) {
+      case 'ADD_ITEM' : 
+        return state.concat([action.val])
+      break;
+      default:
+        return state;
+      break;
+    }
+  }
+  var store = createStore(reducer);
+  t.deepEquals(store.getState(),[])
+  store.dispatch({type:'ADD_ITEM', val:3})
+  t.deepEquals(store.getState(), [3])  
+  store.dispatch({type:'ADD_ITEM', val:5})
+  t.deepEquals(store.getState(), [3,5])  
+})
+
+
+
+
+test.only('getState - dispatch test with combineReducers', function(t) {
+  var reducer = function(state,action) {
+    if (state === undefined) {
+      state = []
+    }
+    switch (action.type) {
+      case 'ADD_ITEM' : 
+        return state.concat([action.val])
+      break;
+      default:
+        return state;
+      break;
+    }
+  }
+  var reducer2 = function(state,action) {
+    if (state === undefined) {
+      state = 0
+    }
+    switch (action.type) {
+      case 'INCR' : 
+        return state+1;
+      break;
+      case 'DOUBLE' : 
+        return state*2;
+      break;
+      default:
+        return state;
+      break;
+    }
+  }
+  var store = createStore(combineReducers({list:reducer,number:reducer2}))
+  t.deepEquals(store.getState(),{list:[],number:0})
+  store.dispatch({type:'ADD_ITEM', val:3})
+  store.dispatch({type:'ADD_ITEM', val:7})
+  store.dispatch({type:'INCR'})
+  store.dispatch({type:'INCR'})
+  store.dispatch({type:'DOUBLE'})
+  t.deepEquals(store.getState(),{list:[3,7],number:4})
+  t.end()
 })
